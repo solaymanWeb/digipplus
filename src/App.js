841e0668b26f87cb84cfa881c84e6951a2635a48
solaymanWeb <solaymanweb27@gmail.com>
2022-11-06 14:33:51 +0600

    header part added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Jobs from './components/Jobs/Jobs';
+import Payment from './components/Payment/Payment';
 
 function App() {
   return (
     <div className="App">
-      Hello
+    <Header></Header>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/jobs' element={<Jobs></Jobs>}></Route>
+        <Route path='/payment' element={<Payment></Payment>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import mainlog from "../pictures/logo.png";
+import rightLogo from "../pictures/right-logo.png"
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar className='header-area' expand="lg">
+                <Container>
+                    <div className="navbar-main">
+                    <div className="nav-img">
+                        <img src={mainlog} alt="logo" />
+                    </div>
+                    <div className="nav-menu">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="">
+                                <Link to="/">Home</Link>
+                                <Link to="/jobs">My Jobs</Link>
+                                <Link to="/payment">My Payment</Link>
+                                <Link to="link">My Offerleeter</Link>
+                                <Link to="link">My Certificate</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                    <div className="nav-button">
+                        <img src={rightLogo} alt="right logo" />
+                    </div>
+                    </div>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
